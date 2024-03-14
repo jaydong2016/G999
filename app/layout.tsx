e74f22a3fcd45dbbcf11e6c37618ac1cb7f31021
dp262 <huangwudong2011@gmail.com>
2024-03-14 22:44:35 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "dongstop.vip",
+  title: "free.644566.xyz",
   description: "www.dongstop.vip",
   viewport: {
     width: "device-width",

0b51745aca3cf09a63f00ecf54355bdcbce35f8e
dp262 <huangwudong2011@gmail.com>
2024-03-27 00:47:38 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -360,7 +360,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "📍 也许能用 gpt-4 的公益站 👉 https://aix.644566.xyz/ \n💡 有什么可以帮你的吗？",
+    BotHello: "💡 有什么可以帮你的吗？",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,

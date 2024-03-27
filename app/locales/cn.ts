231da278beeb79122f7b94ec8b60fd7c00fe89bf
dp262 <huangwudong2011@gmail.com>
2024-03-28 00:38:56 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -360,7 +360,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: "有什么可以帮你的吗？",
+    BotHello: "主用网址：https://999.dongstop.vip/ | 备用：https://chat.644566.xyz/ \n有什么可以帮你的吗？",
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,

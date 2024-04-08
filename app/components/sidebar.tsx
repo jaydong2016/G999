8d8127f2c53683a7f2dc3efc1bbac6ccad1338a4
dp262 <huangwudong2011@gmail.com>
2024-04-08 23:56:59 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -157,9 +157,11 @@ export function SideBar(props: { className?: string }) {
           GPT￥9.99
         </div>
         <div className={styles["sidebar-sub-title"]}>
-         <div className={styles["sidebar-sub-title"]} style={{color: '#af5ded'}}>
-          请勿公开分享本站{"\n"}乱码解决：刷新+新建聊天
-         </div>
+        <div className={styles["sidebar-sub-title"]} style={{color: '#af5ded'}}>
+            请勿公开分享本站
+        </div>
+        <div className={styles["sidebar-sub-title"]} style={{color: '#af5ded'}}>
+            乱码解决：刷新+新建聊天 
         </div>
 
         <div className={styles["sidebar-logo"] + " no-dark"}>
